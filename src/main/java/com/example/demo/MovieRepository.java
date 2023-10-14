23dc5e5b54f4437ab9a3f1dabbd61f90933ef8f2
ravduda <ravduda@gmail.com>
2023-10-14 19:57:41 +0200

    Added put

--- src/main/java/com/example/demo/MovieRepository.java
+++ src/main/java/com/example/demo/MovieRepository.java
@@ -13,11 +13,24 @@ public class MovieRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public List<Movie> getAll(){
+    public List<Movie> getAll() {
         return jdbcTemplate.query("SELECT id, name, rating FROM movie", BeanPropertyRowMapper.newInstance(Movie.class));
     }
-    public Movie getById(int id){
+
+    public Movie getById(int id) {
         return jdbcTemplate.queryForObject("SELECT * FROM movie WHERE " +
                 "id=?", BeanPropertyRowMapper.newInstance(Movie.class), id);
+    }
+
+    public int save(Movie movie) {
+        jdbcTemplate.update("INSERT INTO movie(name, rating) VALUES(?, ?)",
+                movie.getName(), movie.getRating());
+
+        return 0;
+    }
+
+    public int update(Movie movie) {
+        return jdbcTemplate.update("UPDATE movie SET name=?, rating=? WHERE id=?",
+                movie.getName(), movie.getRating(), movie.getId());
     }
 }
